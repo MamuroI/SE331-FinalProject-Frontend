db9fccf92a98f83782349b865c8c6b8addbb31a6
Pongpanoth Panya <pongpanoth213@gmail.com>
2021-10-27 11:12:09 +0700

    add reactive and dockerfile

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SE331-FinalProject-Frontend
+
+Contributors:
+    <br/>
+    `622115014 Thanatorn Kanthala  https://github.com/aisudev`
+    <br/>
+    `622115024 Pongpanoth Panya https://github.com/MamuroI`
+    <br/>
+
+    
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,3 @@
 import { reactive } from "vue"
 
-export default reactive({ flashMessage: '', patients: null, selectedPatient: null })
+export default reactive({ flashMessage: '', patients: null, selectedPatient: null, currentUser: JSON.parse(localStorage.getItem('user')) })
